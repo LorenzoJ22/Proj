bd368eafe00416469a85fa0ad5bb5cd3bbe6aa23
LorenzoJ22 <lorenzo3tenore2@gmail.com>
2025-11-25 17:16:52 +0100

    Tolto il 2

--- Prova.c
+++ Prova.c
@@ -55,7 +55,7 @@ void main()
         /* inbuf still contains the null terminator, so printf prints correctly */
         printf("%s\n", inbuf);              
     }
-    /*Ecco la prova 2*/
+    /*Ecco la prova */
     int x=0;
     exit(1);
 }
