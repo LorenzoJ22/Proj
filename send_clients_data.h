62a3c6a70a7209333efa88a2c3804fda5409bc9a
AleUmi <marepersonale@gmail.com>
2026-02-11 00:32:24 +0000

    warning fix

--- send_clients_data.h
+++ send_clients_data.h
@@ -3,8 +3,8 @@
 
 #include "network.h"
 
-void client_write_data(int sockfd, char *buffer);
-void client_read_data(int sockfd, char *buffer);
+void client_write_data(int sockfd);
+void client_read_data(int sockfd);
 void client_upload(int sockfd, char* buffer, char* ip, int port, char* current_username);
 void client_download(int sockfd, char* buffer, char* ip, int port, char* current_username);
 
